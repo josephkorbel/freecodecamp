f3c2db5539e01ba28e5056a7c17881491340f476
Joe Korbel <korbel23@gmail.com>
2018-06-25 15:23:02 -0400

    Add files via upload

---
+++ Exercises/ToggleWithState.js
@@ -0,0 +1,28 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+  toggleVisibility(){
+    return this.setState(prevState => ({visibility : !prevState.visibility}))
+  }
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+};
